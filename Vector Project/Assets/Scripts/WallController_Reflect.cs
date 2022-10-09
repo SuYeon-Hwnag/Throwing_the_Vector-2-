0bb0aba4c2d2fffb7d84a7ecc8ad2f474cd5c6f0
SuYeon-Hwnag <109446753+SuYeon-Hwnag@users.noreply.github.com>
2022-10-09 17:54:42 +0900

    1009

--- Vector Project/Assets/Scripts/WallController_Reflect.cs
+++ Vector Project/Assets/Scripts/WallController_Reflect.cs
@@ -8,6 +8,7 @@ public class WallController_Reflect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Vector3.Reflect
     }
 
     // Update is called once per frame
@@ -22,14 +23,25 @@ public class WallController_Reflect : MonoBehaviour
         Vector2 b = a + 2 * p;
         return b;
     }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Arrow"))
         {
+            //Debug.Log("collision");
+            //Rigidbody2D ArrowRB_Wall = collision.gameObject.GetComponent<Rigidbody2D>();
+            //Vector2 move = collision.gameObject.GetComponent<ArrowController>().move;
+            //ArrowRB_Wall.velocity = Reflect(move, -collision.GetContact(0).normal); // GetContact 충돌 접점의 정보, normal 법선
+
+            //var speed = collision.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+            //var dir = Vector2.Reflect(collision.gameObject.GetComponent<Rigidbody2D>().velocity.normalized, collision.contacts[0].normal);
+
+            //collision.gameObject.GetComponent<Rigidbody2D>().velocity = dir * Mathf.Max(speed, 0f);
+            //Debug.Log("collision");
+
+            collision.gameObject.GetComponent<ArrowController>().move = Quaternion.AngleAxis(180, collision.contacts[0].normal) * collision.gameObject.GetComponent<Transform>().forward * -1;
             Debug.Log("collision");
-            Rigidbody2D ArrowRB_Wall = collision.gameObject.GetComponent<Rigidbody2D>();
-            Vector2 move = collision.gameObject.GetComponent<ArrowController>().move;
-            ArrowRB_Wall.velocity = Reflect(move, -collision.GetContact(0).normal); // GetContact 충돌 접점의 정보, normal 법선
         }
     }
+
 }
